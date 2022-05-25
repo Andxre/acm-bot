854e6b6f465b7aba1a852203e920df2acadb9c9f
Andre Dasalla <apd6710@gmail.com>
2022-05-25 16:22:07 -0700

    Pinger

--- cogs/timedEvents.py
+++ cogs/timedEvents.py
@@ -1,16 +1,33 @@
 import discord
 from discord.ext import commands
+from discord.ext import tasks
 from discord.ext.commands.core import has_permissions
 import re
 import os
 
-class TimedEvents(commands.cog):
+class TimedEvents(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.pingD.start()
 
-'''
-    @tasks.loop(minutes = 60)
+    @tasks.loop(minutes = 720)
     async def pingD(self):
-        userId = ""
-        await       
-'''
+        userId = 755682652698443907
+        channelId = 882336112050339890
+        channel = self.bot.get_channel(channelId)
+        msg = await channel.send(f'<@{userId}>')
+        await msg.delete()
+
+    @commands.command()
+    async def stopPing(self, ctx):
+        self.pingD.cancel()
+        await ctx.channel.send("Disabled Pinging")
+    
+    @commands.command()
+    async def startPing(self, ctx):
+        self.pingD.start()
+        await ctx.channel.send("Enabled Pinging")
+    
+
+def setup(bot):
+    bot.add_cog(TimedEvents(bot))
